87362f2e0a63374832723a7cdbe60f5bc101aaf0
Addison Guskey <addisonguskey@Addisons-MacBook-Pro.local>
2022-07-14 12:35:37 -0600

    headerjs

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,13 +1,15 @@
 import React from "react";
-import "../style.css";
-import Image from "../assets/images/logo-1.png";
+// import "../style.css";
+import Image from "../assets/images/logo.png";
 
 export default function Header() {
   return (
-    <div>
-      <a href="/">
-        <img style={{ height: "6rem" }} id="logo" src={Image} alt="logo" />
-      </a>
+    <div className="d-flex flex-column justify-content-between flex-md-row align-items-center p-3 px-md-4 mb-3">
+      <h5 className="my-0 mr-md-auto font-weight-normal">
+        <a href="/">
+          <img style={{ height: "6rem" }} id="logo" src={Image} alt="logo" />
+        </a>
+      </h5>
     </div>
   );
 }
