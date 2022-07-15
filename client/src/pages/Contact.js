87dae485384a267d82ad3391d3cd44aee7c9e461
rrkincaid <rrkincaid@gmail.com>
2022-07-15 13:14:20 -0600

    adding pages and updating routes

---
+++ client/src/pages/Contact.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "../style.css";
+
+export default function Contact() {
+  return (
+    <div>
+      <div>
+        <h3>
+          Have Questions, or just wish to connect with our team? Please fill out
+          the fields below and we will be in touch as soon as we can!
+        </h3>
+      </div>
+
+      {/* <div>
+        <h1>FLAVORITES OF THE WEEK </h1>
+      </div> */}
+    </div>
+  );
+}
