f7260ac78e9cbce2017c5469ec939842807c73f2
Ryan Brisch <brischster@gmail.com>
2022-07-15 16:35:36 -0600

    bugs indes and backend is running

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,10 +3,10 @@ const mongoose = require("mongoose");
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost:27017/meltedCone",
   {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true,
+    // useCreateIndex: true,
+    // useFindAndModify: false,
   }
 );
 
