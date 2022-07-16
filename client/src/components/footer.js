4cb7961d6b140f2da5f29680e128ea24494ffdcd
Addison Guskey <addisonguskey@Addisons-MacBook-Pro.local>
2022-07-16 08:27:08 -0600

    made new logo, and a logo for dark mode, som minor styling changes

--- client/src/components/footer.js
+++ client/src/components/footer.js
@@ -1,13 +1,16 @@
 import React from "react";
 // import "../style.css";
-import image from "../assets/images/logo.png";
+import image from "../assets/images/dark-logo.png";
 
 export default function Footer() {
   return (
-    <div>
-      <a href="/">
-        <img style={{ height: "10rem" }} id="logo" src={image} alt="logo" />
-      </a>
+    <div className="row ">
+      <div className="d-flex flex-column text-center">
+        <p>☻ the MELTED CONE, LLC</p>
+        <a href="/">
+          <img style={{ height: "3rem" }} id="logo" src={image} alt="logo" />
+        </a>
+      </div>
     </div>
   );
 }
