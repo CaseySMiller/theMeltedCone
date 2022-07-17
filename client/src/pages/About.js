2eb76002d8eb99e4146c71a5f51e6f90b1f58c46
rrkincaid <rrkincaid@gmail.com>
2022-07-16 21:50:04 -0600

    bringing style aspects to contact and about

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,22 +1,49 @@
 import React from "react";
 import "../style.css";
 
+const styles = {
+  container: {
+    backgroundColor: "#FFD6E8",
+  },
+  img: {
+    height: "35rem",
+    width: "35rem",
+  },
+  quote: {
+    color: "white",
+    fontFamily: "Syncopate",
+    fontDisplay: "sans-serif",
+    textAlign: "center",
+  },
+  span: {
+    color: "#FF007F",
+  },
+  btn: {
+    textDecoration: "none",
+    color: "#FF007F",
+  },
+  flavorites: {
+    marginTop: 45,
+    color: "#FF007F",
+    fontFamily: "Syncopate",
+    fontDisplay: "sans-serif",
+    textAlign: "center",
+  },
+};
 export default function About() {
   return (
-    <div>
-      <div>
+    <div className="row p-5 justify-content-around" style={styles.container}>
+      <div style={styles.quote}>
         <h3>
           Make life even sweeter here at the Melted Cone. Subscribe and cool off
-          with a sweet treat delivered right to your door. Regardless of the
-          weather, ice cream is the perfect treat for any occassion and with us
-          you can try new flavors without the hassle. Share with your family or
-          eat it yourself (we dont judge!)
+          with a sweet treat delivered right to your door.
+        </h3>
+        <h3>
+          Regardless of the weather, ice cream is the perfect treat for any
+          occassion and with us you can try new flavors without the hassle.
+          Share with your family or eat it yourself (we dont judge!)
         </h3>
       </div>
-
-      {/* <div>
-        <h1>FLAVORITES OF THE WEEK </h1>
-      </div> */}
     </div>
   );
 }
