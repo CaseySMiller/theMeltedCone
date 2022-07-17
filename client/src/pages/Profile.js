f1b98436ed411cb7bc1d803458f26d0d6375a556
rrkincaid <rrkincaid@gmail.com>
2022-07-17 15:40:42 -0600

    saved styling

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -6,15 +6,31 @@ const styles = {
     height: "14rem",
     width: "15rem",
   },
+  quote: {
+    color: "white",
+    fontFamily: "Syncopate",
+    fontDisplay: "sans-serif",
+    textAlign: "center",
+  },
   card: {
     maxWidth: "28rem",
+  },
+  container: {
+    backgroundColor: "#FFD6E8",
+  },
+  flavorites: {
+    marginTop: 45,
+    color: "#FF007F",
+    fontFamily: "Syncopate",
+    fontDisplay: "sans-serif",
+    textAlign: "center",
   },
 };
 
 export default function Profile() {
   return (
-    <div className="row p-5 justify-content-around">
-      <h1> YOUR PAST ORDERS</h1>
+    <div className="row p-5 justify-content-around" style={styles.container}>
+      <h1 style={styles.quote}> YOUR PAST ORDERS</h1>
       {/* card 1 */}
       <div className="d-flex flex-column my-5 col-xl-3 col-lg-4 col-md-5 col-sm-6">
         <div
@@ -23,14 +39,20 @@ export default function Profile() {
         >
           <img style={styles.scoop} src={Image} className="card-img-top" />
           <div className="card-body">
-            <h5 className="card-title"> ICE CREAM FLAVOR</h5>
+            <h5 className="card-title" style={styles.flavorites}>
+              {" "}
+              ICE CREAM FLAVOR
+            </h5>
             <p className="card-text">
               This flavor was enjoyed in your previous order, click below to add
               to your current order so you can enjoy this flavor again
             </p>
           </div>
           <div className="card-body">
-            <a className="card-link"> ORDER AGAIN? </a>
+            <a className="card-link" style={styles.flavorites}>
+              {" "}
+              ORDER AGAIN?{" "}
+            </a>
           </div>
         </div>
       </div>
@@ -42,14 +64,20 @@ export default function Profile() {
         >
           <img style={styles.scoop} src={Image} className="card-img-top" />
           <div className="card-body">
-            <h5 className="card-title"> ICE CREAM FLAVOR</h5>
+            <h5 className="card-title" style={styles.flavorites}>
+              {" "}
+              ICE CREAM FLAVOR
+            </h5>
             <p className="card-text">
               This flavor was enjoyed in your previous order, click below to add
               to your current order so you can enjoy this flavor again
             </p>
           </div>
           <div className="card-body">
-            <a className="card-link"> ORDER AGAIN? </a>
+            <a className="card-link" style={styles.flavorites}>
+              {" "}
+              ORDER AGAIN?{" "}
+            </a>
           </div>
         </div>
       </div>
@@ -61,14 +89,20 @@ export default function Profile() {
         >
           <img style={styles.scoop} src={Image} className="card-img-top" />
           <div className="card-body">
-            <h5 className="card-title"> ICE CREAM FLAVOR</h5>
+            <h5 className="card-title" style={styles.flavorites}>
+              {" "}
+              ICE CREAM FLAVOR
+            </h5>
             <p className="card-text">
               This flavor was enjoyed in your previous order, click below to add
               to your current order so you can enjoy this flavor again
             </p>
           </div>
           <div className="card-body">
-            <a className="card-link"> ORDER AGAIN? </a>
+            <a className="card-link" style={styles.flavorites}>
+              {" "}
+              ORDER AGAIN?{" "}
+            </a>
           </div>
         </div>
       </div>
@@ -80,18 +114,24 @@ export default function Profile() {
         >
           <img style={styles.scoop} src={Image} className="card-img-top" />
           <div className="card-body">
-            <h5 className="card-title"> ICE CREAM FLAVOR</h5>
+            <h5 className="card-title" style={styles.flavorites}>
+              {" "}
+              ICE CREAM FLAVOR
+            </h5>
             <p className="card-text">
               This flavor was enjoyed in your previous order, click below to add
               to your current order so you can enjoy this flavor again
             </p>
           </div>
           <div className="card-body">
-            <a className="card-link"> ORDER AGAIN? </a>
+            <a className="card-link" style={styles.flavorites}>
+              {" "}
+              ORDER AGAIN?{" "}
+            </a>
           </div>
         </div>
       </div>
-      <h1> YOUR FLAVORITES </h1>
+      <h1 style={styles.quote}> YOUR FLAVORITES </h1>
       {/* card 5 */}
       <div className="d-flex flex-column my-5 col-xl-3 col-lg-4 col-md-5 col-sm-6">
         <div
@@ -100,15 +140,24 @@ export default function Profile() {
         >
           <img style={styles.scoop} src={Image} className="card-img-top" />
           <div className="card-body">
-            <h5 className="card-title"> ICE CREAM FLAVOR</h5>
+            <h5 className="card-title" style={styles.flavorites}>
+              {" "}
+              ICE CREAM FLAVOR
+            </h5>
             <p className="card-text">
               This flavor was enjoyed in your previous order, click below to add
               to your current order so you can enjoy this flavor again
             </p>
           </div>
           <div className="card-body">
-            <a className="card-link"> ORDER AGAIN? </a>
-            <a className="card-link"> REMOVE FROM FLAVORITES? </a>
+            <a className="card-link" style={styles.flavorites}>
+              {" "}
+              ORDER AGAIN?{" "}
+            </a>
+            <a className="card-link" style={styles.flavorites}>
+              {" "}
+              REMOVE FROM FLAVORITES?{" "}
+            </a>
           </div>
         </div>
       </div>
@@ -120,15 +169,24 @@ export default function Profile() {
         >
           <img style={styles.scoop} src={Image} className="card-img-top" />
           <div className="card-body">
-            <h5 className="card-title"> ICE CREAM FLAVOR</h5>
+            <h5 className="card-title" style={styles.flavorites}>
+              {" "}
+              ICE CREAM FLAVOR
+            </h5>
             <p className="card-text">
               This flavor was enjoyed in your previous order, click below to add
               to your current order so you can enjoy this flavor again
             </p>
           </div>
           <div className="card-body">
-            <a className="card-link"> ORDER AGAIN? </a>
-            <a className="card-link"> REMOVE FROM FLAVORITES? </a>
+            <a className="card-link" style={styles.flavorites}>
+              {" "}
+              ORDER AGAIN?{" "}
+            </a>
+            <a className="card-link" style={styles.flavorites}>
+              {" "}
+              REMOVE FROM FLAVORITES?{" "}
+            </a>
           </div>
         </div>
       </div>
@@ -140,15 +198,24 @@ export default function Profile() {
         >
           <img style={styles.scoop} src={Image} className="card-img-top" />
           <div className="card-body">
-            <h5 className="card-title"> ICE CREAM FLAVOR</h5>
+            <h5 className="card-title" style={styles.flavorites}>
+              {" "}
+              ICE CREAM FLAVOR
+            </h5>
             <p className="card-text">
               This flavor was enjoyed in your previous order, click below to add
               to your current order so you can enjoy this flavor again
             </p>
           </div>
           <div className="card-body">
-            <a className="card-link"> ORDER AGAIN? </a>
-            <a className="card-link"> REMOVE FROM FLAVORITES? </a>
+            <a className="card-link" style={styles.flavorites}>
+              {" "}
+              ORDER AGAIN?{" "}
+            </a>
+            <a className="card-link" style={styles.flavorites}>
+              {" "}
+              REMOVE FROM FLAVORITES?{" "}
+            </a>
           </div>
         </div>
       </div>{" "}
@@ -160,15 +227,24 @@ export default function Profile() {
         >
           <img style={styles.scoop} src={Image} className="card-img-top" />
           <div className="card-body">
-            <h5 className="card-title"> ICE CREAM FLAVOR</h5>
+            <h5 className="card-title" style={styles.flavorites}>
+              {" "}
+              ICE CREAM FLAVOR
+            </h5>
             <p className="card-text">
               This flavor was enjoyed in your previous order, click below to add
               to your current order so you can enjoy this flavor again
             </p>
           </div>
           <div className="card-body">
-            <a className="card-link"> ORDER AGAIN? </a>
-            <a className="card-link"> REMOVE FROM FLAVORITES? </a>
+            <a className="card-link" style={styles.flavorites}>
+              {" "}
+              ORDER AGAIN?{" "}
+            </a>
+            <a className="card-link" style={styles.flavorites}>
+              {" "}
+              REMOVE FROM FLAVORITES?{" "}
+            </a>
           </div>
         </div>
       </div>
