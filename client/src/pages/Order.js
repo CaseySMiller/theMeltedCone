e6ae9f51c37629917aad7d34c1007897750edceb
Addison Guskey <addisonguskey@Addisons-MacBook-Pro.local>
2022-07-17 12:04:20 -0600

    what I have so far on order

--- client/src/pages/Order.js
+++ client/src/pages/Order.js
@@ -27,59 +27,36 @@ export default function Order() {
               bulk of the card's content.
             </p>
           </div>
-          <ul className="list-group list-group-flush">
-            <li className="list-group-item">An item</li>
-            <li className="list-group-item">A second item</li>
-            <li className="list-group-item">A third item</li>
-          </ul>
-          <div className="card-body">
-            {/* MODAL BUTTON  */}
+          <div class="col-md-3 col-lg-3 col-xl-2 d-flex">
             <button
-              type="button"
-              className="btn btn-primary"
-              data-bs-toggle="modal"
-              data-bs-target="#exampleModal"
+              class="btn btn-link px-2"
+              onclick="this.parentNode.querySelector('input[type=number]').stepDown()"
             >
-              Launch demo modal
+              <i class="fas fa-minus"></i>
             </button>
 
-            {/* MODAL */}
-            <div
-              className="modal fade"
-              id="exampleModal"
-              tabindex="-1"
-              aria-labelledby="exampleModalLabel"
-              aria-hidden="true"
+            <input
+              id="form1"
+              min="0"
+              name="quantity"
+              value="1"
+              type="number"
+              class="form-control form-control-sm"
+            />
+
+            <button
+              class="btn btn-link px-2"
+              onclick="this.parentNode.querySelector('input[type=number]').stepUp()"
             >
-              <div className="modal-dialog">
-                <div className="modal-content">
-                  <div className="modal-header">
-                    <h5 className="modal-title" id="exampleModalLabel">
-                      Modal title
-                    </h5>
-                    <button
-                      type="button"
-                      className="btn-close"
-                      data-bs-dismiss="modal"
-                      aria-label="Close"
-                    ></button>
-                  </div>
-                  <div className="modal-body">...</div>
-                  <div className="modal-footer">
-                    <button
-                      type="button"
-                      className="btn btn-secondary"
-                      data-bs-dismiss="modal"
-                    >
-                      Close
-                    </button>
-                    <button type="button" className="btn btn-primary">
-                      Save changes
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </div>
+              <i class="fas fa-plus"></i>
+            </button>
+          </div>
+          <div class="col-md-3 col-lg-2 col-xl-2 offset-lg-1">
+            <h6 class="mb-0">€ 44.00</h6>
+          </div>
+          <div className="card-body">
+            <a className="card-link">Card link</a>
+            <a className="card-link">Another link</a>
           </div>
         </div>
       </div>
