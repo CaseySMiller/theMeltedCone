6a80d956aecf36872648f039633466ebcc902f65
rrkincaid <rrkincaid@gmail.com>
2022-07-17 16:25:46 -0600

    card text

--- client/src/pages/Subscribe.js
+++ client/src/pages/Subscribe.js
@@ -1,15 +1,24 @@
 import React from "react";
 import "../style.css";
 
+const styles = {
+  container: {
+    backgroundColor: "#FFD6E8",
+  },
+  flavorites: {
+    marginTop: 45,
+    color: "#FF007F",
+    fontFamily: "Syncopate",
+    fontDisplay: "sans-serif",
+    textAlign: "center",
+  },
+};
 export default function Subscribe() {
   return (
     <div>
       <div>
-        <h3>
-          Subscribe Here  
-        </h3>
+        <h3 style={styles.flavorites}>Subscribe Here</h3>
       </div>
-
     </div>
   );
 }
