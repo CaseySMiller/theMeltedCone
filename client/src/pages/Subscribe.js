5102bdd4c573b03b5c891864187f0b20d3132f7a
rrkincaid <rrkincaid@gmail.com>
2022-07-19 17:02:59 -0600

    seeded product data

--- client/src/pages/Subscribe.js
+++ client/src/pages/Subscribe.js
@@ -43,7 +43,7 @@ export default function Subscribe() {
               <div className="card-body" style={styles.container}>
                 <h5 className="card-title" style={styles.flavorites}>
                   {" "}
-                  featured flavorite of the month
+                  currernt - featured flavorite of the month
                 </h5>
                 <h5 className="card-title" style={styles.flavorites}>
                   {" "}
